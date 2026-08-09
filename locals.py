5adefa5e8a6cb4bc87af2d343fc6b07c091c7b15
hengdapi <hengxiaopi@gmail.com>
2026-08-09 22:33:27 +0800

    更新版本号v1.1.1

--- locals.py
+++ locals.py
@@ -12,7 +12,7 @@ from PySide6.QtCore import QTimer
 from qfluentwidgets_pro import TableWidget,PrimaryPushButton,PushButton,FluentIcon,TextEdit
 
 from wr_settings import *
-app_version="v1.1.0"
+app_version="v1.1.1"
 logging.basicConfig(format="[%(levelname)s] %(asctime)s %(filename)s %(funcName)s %(lineno)d行:\t%(message)s",
                     level=logging.INFO,
                     filename=None,
